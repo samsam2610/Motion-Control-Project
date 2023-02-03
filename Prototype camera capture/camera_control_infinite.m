3ef808c4ff2aca3cbb40ab72eca584fae9654727
Sam Tran <quocsam93@gmail.com>
2023-02-03 14:44:15 -0600

    beta camera capture

--- Prototype camera capture/camera_control_infinite.m
+++ Prototype camera capture/camera_control_infinite.m
@@ -1,10 +1,12 @@
-%% This file is to test on the treadmill/TDT computer - with the nidaq attached. 
+%% This file is to test on the treadmill/TDT computer - using TDT pulse as trigger. 
 %% The test is about the accuracy of the absolute timestamp
 
 %% Each run unique ID
 TIME_START = string(datetime('now', 'Format','dd_MMM_yyyy-HH_mm_ss'));
 
 %% Setting parameters
+SYSTEM_NAME = 'NIDAQ'; % type "NIDAQ" or "TDT" for trigger type
+
 DAQ_RATE = 1000;
 DAQ_CHANNEL = [1];
 DAQ_DEVICE_NAME = "Dev1";
@@ -15,32 +17,45 @@ CAMERA_EXPOSURE = 1/2^9;
 CAMERA_GAIN = 22;
 CAMERA_FRAMERATE = 200;
 
-VIDEO_TIME_RECORD = 40; % seconds
+VIDEO_TIME_RECORD = 3600; % seconds
 VIDEO_FRAMERATE = 200; % fps
 VIDEO_WIDTH = 785; 
 VIDEO_HEIGHT = 232;
 
 TIME_TABLE_OFFSET = 0.1; % offset 0 to a percentage of framerate - guessed number - not sure
 
-VIDEO_EXPORT = false;
+VIDEO_EXPORT = true;
 VIDEO_NAMES = ["Camera_1", "Camera_2"];
 for indexName = 1:length(VIDEO_NAMES)
     VIDEO_NAMES(indexName) = VIDEO_NAMES(indexName) + "_" + TIME_START;
 end
 
-%% Setup trigger object
-deviceObject = daq('ni');
+%% Setup TDT/NIDAQ connection
+switch SYSTEM_NAME
+case 'TDT'
+    addpath(genpath('C:\TDT\TDTMatlabSDK'));
+    syn = SynapseAPI('localhost');
 
-deviceObject.Rate = DAQ_RATE; % Set acquisition rate, in scans/second
-deviceName = DAQ_DEVICE_NAME; % Set the device name
-unitName = DAQ_UNIT_NAME; % Unit of the measurement
-channelNumbers = DAQ_CHANNEL; % Channel to record
+case 'NIDAQ'
+    deviceObject = daq('ni');
 
-for index = 1:length(channelNumbers)
-    channelNumber = channelNumbers(index);
-    channel = addinput(deviceObject, deviceName, "ai" + num2str(channelNumber), unitName);
-    channel.TerminalConfig = 'SingleEnded';
+    deviceObject.Rate = DAQ_RATE; % Set acquisition rate, in scans/second
+    deviceName = DAQ_DEVICE_NAME; % Set the device name
+    unitName = DAQ_UNIT_NAME; % Unit of the measurement
+    channelNumbers = DAQ_CHANNEL; % Channel to record
+
+    for index = 1:length(channelNumbers)
+        channelNumber = channelNumbers(index);
+        channel = addinput(deviceObject, deviceName, "ai" + num2str(channelNumber), unitName);
+        channel.TerminalConfig = 'SingleEnded';
+    end
 end
+
+
+%% Create the waitbar for the cancelation button
+hWaitBar = waitbar(0, 'Recording progress', 'CreateCancelBtn', ...
+                   @(src, event) setappdata(gcbf(), 'Cancelled', true));
+setappdata(hWaitBar, 'Cancelled', false);
 
 %% Setup camera object
 % Create connection to the device using the specified adaptor with the specified 
@@ -86,25 +101,43 @@ triggerUpdate_2 = parallel.pool.PollableDataQueue;
 terminationUpdate_1 = parallel.pool.PollableDataQueue;
 terminationUpdate_2 = parallel.pool.PollableDataQueue;
 
+tic
+
 % Start the workers and send the parameters
 f(1:2) = parallel.FevalFuture;
-f(1) = parfeval(@captureImage, ... % name of the function to send to the worker
+f(1) = parfeval(@saveCaptureImage, ... % name of the function to send to the worker
                 2, ... % number of expecting outputs
                 videoObject_1, ...
                 triggerUpdate_1, ...
                 terminationUpdate_1, ...
                 frame_rate, ...
-                time_record, ...
-                time_table_promise);
+                time_table_promise, ...
+                VIDEO_NAMES(1));
 
-f(2) = parfeval(@captureImage, ... % name of the function to send to the worker
+f(2) = parfeval(@saveCaptureImage, ... % name of the function to send to the worker
                 2, ... % number of expecting outputs
                 videoObject_2, ...
                 triggerUpdate_2, ...
                 terminationUpdate_2, ...
                 frame_rate, ...
-                time_record, ...
-                time_table_promise);
+                time_table_promise, ...
+                VIDEO_NAMES(2));
+
+% Acquire pollable terminations from the cores
+pause(2)
+terminationStatus_1 = poll(terminationUpdate_1);
+terminationStatus_2 = poll(terminationUpdate_2);
+while (1)
+    if ~isa(terminationStatus_1, class(terminationUpdate_1))
+        terminationStatus_1 = poll(terminationUpdate_1);
+    end
+    if ~isa(terminationStatus_2, class(terminationUpdate_2))
+        terminationStatus_2 = poll(terminationUpdate_2);
+    end
+    if isa(terminationStatus_1, class(terminationUpdate_1)) && isa(terminationStatus_2, class(terminationUpdate_2))
+        break
+    end
+end
 
 % Acquire pollable triggers from the cores
 pause(2)
@@ -123,43 +156,45 @@ while (1)
 end
 beta = 1;
 
-% send trigger info to the camera to start capturing
-deviceObject.ScansAvailableFcn = @(src,event) triggerDetect(src, deviceObject, triggerStatus_1, triggerStatus_2);
-start(deviceObject, 'continuous');
+switch SYSTEM_NAME
+case 'TDT'
+    % Read the pulse from TDT info to the camera to start capturing
+    while (1)
+        params = syn.getParameterValue('PulseGen1', 'out_FloatOut');
+        if params > 1
+            datetime_current = datetime('now', 'Format','dd-MMM-yyyy HH:mm:ss.SSSSSSSSS');
+            send(triggerStatus_1, datetime_current);
+            send(triggerStatus_2, datetime_current);
+            break
+        end
+    end
 
-wait(f)
+case 'NIDAQ'
+    % send trigger info to the camera to start capturing
+    deviceObject.ScansAvailableFcn = @(src,event) triggerDetect(src, deviceObject, triggerStatus_1, triggerStatus_2);
+    start(deviceObject, 'continuous');
+
+end
+
+
+current_running_time = toc;
+while current_running_time < VIDEO_TIME_RECORD
+    current_running_time = toc;
+    % Check to see if the cancel button was pressed.
+    if getappdata(hWaitBar, 'Cancelled')
+        datetime_current = datetime('now', 'Format','dd-MMM-yyyy HH:mm:ss.SSSSSSSSS');
+        fprintf('Turning off the recording cancelled.\n');
+        send(terminationStatus_1, datetime_current);
+        send(terminationStatus_2, datetime_current);
+        delete(hWaitBar);
+        break;
+    end
+    waitbar(current_running_time/VIDEO_TIME_RECORD);
+end
+
 
 [time_table_1, snapshot_store_1] = fetchOutputs(f(1));
 [time_table_2, snapshot_store_2] = fetchOutputs(f(2));
-
-%% Exporting the recorded videos
-if VIDEO_EXPORT
-    % Set your file name, video compression, and frame rate
-    video_name = VIDEO_NAMES(1) + ".avi";
-    videoExport_1 = VideoWriter(video_name, 'Grayscale AVI'); 
-    videoExport_1.FrameRate = VIDEO_FRAMERATE; 
-    open(videoExport_1);
-    for indexFrame = 1:size(snapshot_store_1, 3)  %T is your "100x150x75" matrix
-        writeVideo(videoExport_1, mat2gray(snapshot_store_1(:, :, indexFrame)));
-    end
-    close(videoExport_1);
-
-    video_name = VIDEO_NAMES(2) + ".avi";
-    videoExport_2 = VideoWriter(video_name, 'Grayscale AVI');
-    videoExport_2.FrameRate = VIDEO_FRAMERATE; 
-    open(videoExport_2);
-    for indexFrame = 1:size(snapshot_store_2, 3)  %T is your "100x150x75" matrix
-        writeVideo(videoExport_2, mat2gray(snapshot_store_2(:, :, indexFrame)));
-    end
-    close(videoExport_2);
-
-    %% Exporting the time table mat file of the recorded videos
-    time_table_name = VIDEO_NAMES(1) + "time_table" + ".mat";
-    save(time_table_name, 'time_table_1');
-
-    time_table_name = VIDEO_NAMES(2) + "time_table" + ".mat";
-    save(time_table_name, 'time_table_2');
-end
 
 %% Cleaning up
 delete(videoObject_1)
---
+++ Prototype camera capture/saveCaptureImage.m
@@ -0,0 +1,113 @@
+%% Unlike captureImage, saveCaptureImage will attempt to write the frames directly into the video files
+function [time_table, snapshot_store] =  saveCaptureImage(videoObject, ...
+                                                      triggerUpdate, ...
+                                                      terminationUpdate, ...
+                                                      frame_rate, ...
+                                                      time_table_promise, ...
+                                                      video_name)
+
+    time_record = 3600; % 1 hour of recording maximum
+    time_table = cell(time_record * frame_rate, 9);
+    snapshot_store = uint8(zeros(232, 784));
+    ncount = 0;    
+    
+    % Prepare the video file
+    video_name_file = video_name + ".avi";
+    videoExport = VideoWriter(video_name_file, 'Motion JPEG AVI'); 
+    v.Quality = 85;
+    videoExport.FrameRate = frame_rate;
+    open(videoExport); 
+
+    % Send back the pollable termination status to the main core
+    terminationStatus = parallel.pool.PollableDataQueue;
+    pause(0.5);
+    send(terminationUpdate, terminationStatus);
+    pause(1);
+
+    % Send back the pollable trigger to the main core
+    triggerStatus = parallel.pool.PollableDataQueue;
+    pause(0.5);
+    send(triggerUpdate, triggerStatus);
+    pause(1);
+
+    while (1)
+        [time_start, ~] = poll(triggerStatus);
+        if isdatetime(time_start) == 1
+            break
+        end
+    end
+
+    % Start the capturing process
+    vidStart = false;
+    time_table_promise = time_start + seconds(time_table_promise);
+    time_intial = time_table_promise(1);
+    index_promise = 1;
+    index_skip = false;
+    WAIT_FACTOR = 0.85;
+    start(videoObject);
+    tic
+    time_start_sys = toc;
+
+    next_frame = time_start_sys + 1/frame_rate;
+    while (1)
+
+        ncount = ncount + 1;
+        if mod(ncount+1, 500)  % Prevent memory leak.
+            flushdata(videoObject); 
+        end
+        
+        % Sam test - don't worry about this
+        time_current = datetime('now', 'Format','dd-MMM-yyyy HH:mm:ss.SSSSSSSSS');
+        time_promise = time_table_promise(index_promise);
+        time_diff = time_current - time_promise; % different between current time and promised time
+        time_diff.Format = 's';
+
+        % 
+        time_current_sys = toc;
+        time_wait = (next_frame - time_current_sys)*WAIT_FACTOR;
+        if time_wait > 0
+            pause(time_wait);
+        end
+
+        time_previous_sys = time_start_sys;
+        time_start_sys = toc;
+
+        [snapshot_store, metadata] = getsnapshot(videoObject);
+        writeVideo(videoExport, mat2gray(snapshot_store(1:2:end, 1:2:end)));
+
+        time_previous = time_start;
+        time_start = datetime(metadata.AbsTime, 'Format','dd-MMM-yyyy HH:mm:ss.SSSSSSSSS');
+        time_table(index_promise, :) = {time2num(time_diff), ...
+                                        time_promise, ...
+                                        time_start, ...
+                                        time2num(time_start-time_promise), ...
+                                        time_wait,...
+                                        time2num(time_promise-time_intial),...
+                                        time2num(time_start-time_intial),...
+                                        time_start_sys-time_previous_sys,... % time between frames according to the system
+                                        time2num(time_start-time_previous) % metadata time between frames 
+                                        };
+
+        next_frame = max([next_frame + 1/frame_rate, time_start_sys + 1/frame_rate]);
+       
+        index_promise = index_promise + 1;
+        index_skip = false;
+        [terminate_status, ~] = poll(terminationStatus);
+        if (time_current_sys > time_record) || (isdatetime(terminate_status) == 1)
+            % close the video file
+            close(videoExport);
+
+            % clean empty row before saving the time table
+            empty_cell = any(cellfun('isempty', time_table), 2);
+            time_table(empty_cell, :) = [];
+            
+            % save the time table
+            time_table_name = video_name + "_time_table" + ".mat";
+            save(time_table_name, 'time_table');
+            break
+        end
+    end
+    delete(imaqfind)
+    disp(ncount)
+    
+end
