844e06131233cc4c9060e9b9febefe2db99adccd
Sam Tran <quocsam93@gmail.com>
2023-02-06 11:49:30 -0600

    minor clean up

--- Prototype camera capture/triggerDetect.m
+++ Prototype camera capture/triggerDetect.m
@@ -1,12 +1,15 @@
 function triggerDetect(src, deviceObject, triggerStatus_1, triggerStatus_2)
 
-    [data, timestamps, ~] = read(src, src.ScansAvailableFcnCount, "OutputFormat", "Matrix");
-    disp(timestamps)
+    [data, timestamps, triggerTime] = read(src, src.ScansAvailableFcnCount, "OutputFormat", "Matrix");
+    
     if any(data >= 1.0)
+        
         datetime_current = datetime('now', 'Format','dd-MMM-yyyy HH:mm:ss.SSSSSSSSS');
         send(triggerStatus_1, datetime_current);
         send(triggerStatus_2, datetime_current);
         stop(deviceObject)
+        disp(data.Properties.CustomProperties.TriggerTime)
+        disp(datetime_current)
     end
     
 end
